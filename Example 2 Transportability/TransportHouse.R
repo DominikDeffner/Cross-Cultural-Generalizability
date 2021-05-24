a5025a32fc9ce5f5d772fe840fb5df2b218ad7f8
DominikDeffner <33870194+DominikDeffner@users.noreply.github.com>
2021-05-24 12:06:31 +0200

    changed stuff

--- Example 2 Transportability/TransportHouse.R
+++ Example 2 Transportability/TransportHouse.R
@@ -24,6 +24,17 @@ data_Phoenix <- data[which(data$fieldid == 3),]
 data_Pune    <- data[which(data$fieldid == 4),]
 
 
+Demo <- array(0, dim = c(6 , 12, 2))
+for (j in sort(unique(data$fieldid))) {
+  for (i in 4:15) {
+    for (g in 1:2) {
+      Demo[j, which(4:14 == i),g] <- length(which(data$fieldid==j & data$age==i & data$gender== g))
+    }
+  }
+}          
+
+
+
 
 
 Demo_Phoenix <- matrix(0, 11, 2)
@@ -42,25 +53,37 @@ for (i in 4:14) {
 #Prepare for stan
 
 d_list_Phoenix <- list(N = nrow(data_Phoenix), 
-                      MA = 11,
-                      age = data_Phoenix$age - 3,
-                      condition = data_Phoenix$condition -1,
-                      outcome = data_Phoenix$choice, 
-                      gender = data_Phoenix$gender, 
-                      P_empirical = t(Demo_Phoenix),
-                      P_other = t(Demo_Pune))
+                       MA = 11,
+                       age = data_Phoenix$age - 3,
+                       condition = data_Phoenix$condition -1,
+                       outcome = data_Phoenix$choice, 
+                       gender = data_Phoenix$gender, 
+                       P_empirical = t(Demo_Phoenix),
+                       P_other = t(Demo_Pune))
 
 
 d_list_Pune <- list(N = nrow(data_Pune), 
-                       MA = 11,
-                       age = data_Pune$age - 3,
-                       condition = data_Pune$condition -1,
-                       outcome = data_Pune$choice, 
-                       gender = data_Pune$gender, 
-                       P_empirical = t(Demo_Pune),
-                       P_other = t(Demo_Phoenix))
+                    MA = 11,
+                    age = data_Pune$age - 3,
+                    condition = data_Pune$condition -1,
+                    outcome = data_Pune$choice, 
+                    gender = data_Pune$gender, 
+                    P_empirical = t(Demo_Pune),
+                    P_other = t(Demo_Phoenix))
 
 
+
+
+d_list <- list(N = nrow(data), 
+               N_pop = length(unique(data$fieldid)),
+               MA = 12,
+               pop_id = data$fieldid,
+               age = data$age - 3,
+               condition = data$condition -1,
+               outcome = data$choice, 
+               gender = data$gender, 
+               P_empirical = t(Demo_Pune),
+               P_other = t(Demo_Phoenix))
 
 
 
@@ -96,8 +119,10 @@ functions{
 
 data {
   int N;
+  int N_pop;
   int MA;
   int age[N];
+  int pop_id[N];
   int condition[N];
   int outcome[N];
   int gender[N];
@@ -106,7 +131,7 @@ data {
   }
 
 parameters {
-  real alpha;
+  real alpha[N_pop];
   real b_prime;
   vector[MA] age_effect;    //Vector for GP age effects
   
@@ -128,7 +153,7 @@ model {
     
 
   for ( i in 1:N ) {
-   p[i] = alpha + (b_prime + age_effect[age[i]]) * condition[i];
+   p[i] = alpha[pop_id[i]] + (b_prime + age_effect[age[i]]) * condition[i];
   }
 
  outcome ~ binomial_logit(1, p);
@@ -138,47 +163,47 @@ model {
 
 
 generated quantities{
-   real expect_pos = 0;
-   real<lower = 0, upper = 1> p_source;  // This is value for p in the source population 
-   real<lower = 0, upper = 1> p_pop;  // This is value for p in the target population 
-   real<lower = 0, upper = 1> p_other;  // This is value for p in the target population 
-
-   int total = 0;
-   vector[MA] pred_p_m;
-   vector[MA] pred_p_f;
-
-   for (a in 1:2)
-        for (b in 1:MA){
-         total += P_empirical[a,b];
-         expect_pos += P_empirical[a,b] * inv_logit(alpha[a] + age_effect[a,b]);
-     }
-     
-   p_source = expect_pos / total;
-   
-    total = 0;
-    expect_pos = 0;
-    
-       for (a in 1:2)
-         for (b in 1:MA){
-          total += P_other[a,b];
-          expect_pos += P_other[a,b] * inv_logit(alpha[a] + age_effect[a,b]);
-      }
-      
-    p_other = expect_pos / total;
-   
-   
-       total = 0;
-    expect_pos = 0;
-    
-       for (a in 1:2)
-         for (b in 1:MA){
-          total += P_Pop[a,b];
-          expect_pos += P_Pop[a,b] * inv_logit(alpha[a] + age_effect[a,b]);
-      }
-      
-    p_pop = expect_pos / total;
-   
-   
+  real expect_pos = 0;
+  real<lower = 0, upper = 1> p_source;  // This is value for p in the source population 
+  real<lower = 0, upper = 1> p_pop;  // This is value for p in the target population 
+  real<lower = 0, upper = 1> p_other;  // This is value for p in the target population 
+  
+  int total = 0;
+  vector[MA] pred_p_m;
+  vector[MA] pred_p_f;
+  
+  for (a in 1:2)
+    for (b in 1:MA){
+      total += P_empirical[a,b];
+      expect_pos += P_empirical[a,b] * inv_logit(alpha[a] + age_effect[a,b]);
+    }
+  
+  p_source = expect_pos / total;
+  
+  total = 0;
+  expect_pos = 0;
+  
+  for (a in 1:2)
+    for (b in 1:MA){
+      total += P_other[a,b];
+      expect_pos += P_other[a,b] * inv_logit(alpha[a] + age_effect[a,b]);
+    }
+  
+  p_other = expect_pos / total;
+  
+  
+  total = 0;
+  expect_pos = 0;
+  
+  for (a in 1:2)
+    for (b in 1:MA){
+      total += P_Pop[a,b];
+      expect_pos += P_Pop[a,b] * inv_logit(alpha[a] + age_effect[a,b]);
+    }
+  
+  p_pop = expect_pos / total;
+  
+  
   pred_p_m = inv_logit(alpha[1] + age_effect[1,]');
   pred_p_f = inv_logit(alpha[2] + age_effect[2,]');
 }
@@ -190,7 +215,10 @@ generated quantities{
 
 
 
+
 library(rstan)
+m <- stan( model_code  = m , data= d_list ,iter = 5000, cores = 4, seed=1, chains=4, control = list(adapt_delta=0.9, max_treedepth = 13))  
+
 m_Phoenix <- stan( model_code  = m2 , data= d_list_Phoenix ,iter = 5000, cores = 4, seed=1, chains=4, control = list(adapt_delta=0.9, max_treedepth = 13))  
 
 
